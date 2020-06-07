b262c8b380930baaab3f785eefabf604f283d3b7
Aleem Ur-Rehman <gm017377@live.reading.ac.uk>
2020-06-07 13:19:34 +0100

    Updates

---
+++ precache-manifest.dbcf7a8268b69fb07e49434f2a0f8163.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a1eae290397e99322d3b78320875a8",
+    "url": "/etha-site/index.html"
+  },
+  {
+    "revision": "f543949b41b504e2f0f6",
+    "url": "/etha-site/static/css/main.f0a34eb8.chunk.css"
+  },
+  {
+    "revision": "1d2d8391933fe37b0702",
+    "url": "/etha-site/static/js/2.9a3a4515.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/etha-site/static/js/2.9a3a4515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f543949b41b504e2f0f6",
+    "url": "/etha-site/static/js/main.5b8e2c9c.chunk.js"
+  },
+  {
+    "revision": "b6c0f0558a4a6ec5911a",
+    "url": "/etha-site/static/js/runtime-main.4994e7de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/etha-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
